3297556116d6c77958f9953309c3395457a6bc91
Rohan Badiger <rohanbadiger4738@gmail.com>
2024-09-15 20:21:19 +0530

    committed

--- Chapter - 3 Conditional Instructions/03_non_zero.c
+++ Chapter - 3 Conditional Instructions/03_non_zero.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
-int main(){
-    
-    if(1){
+int main()
+{
+
+    if (1)
+    {
         printf("This if is executed!\n");
     }
-    if(2345){
+    if (2345)
+    {
         printf("This if is also executed!\n");
     }
-    if(2.74){
+    if (2.74)
+    {
         printf("This if is also executed!\n");
     }
-    if('c'){
+    if ('c')
+    {
         printf("This character inside if is also executed!\n");
     }
-    if(0){
+    if (0)
+    {
         printf("I am zero - I am not executed\n");
     }
     return 0;
--- Chapter - 3 Conditional Instructions/05_else_if.c
+++ Chapter - 3 Conditional Instructions/05_else_if.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int age = 45;
 
-    if(age>60){
+    if (age > 60)
+    {
         printf("You can drive and you are a senior citizen");
     }
-    else if(age>40){
+    else if (age > 40)
+    {
         printf("You can drive and you are elder");
-    } 
-    else if(age>18){
+    }
+    else if (age > 18)
+    {
         printf("You can drive");
     }
-    else{
+    else
+    {
         printf("You cannot drive");
     }
-
 
     return 0;
 }
